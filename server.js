f5c9d5a38527c548c759d592bb74e0d387b799d7
mmfx001 <dilbekshermatov@gmail.com>
2024-09-04 15:49:31 +0500

    done

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-useEffect(() => {
-  const fetchCategories = async () => {
-    try {
-      const response = await fetch('http://localhost:5000/categories'); // 'Categories' o'rniga 'categories'
-      const data = await response.json();
-      console.log(data); // Ma'lumotlarni konsolda ko'rsatish
-      setCategories(data.categories || []);
-    } catch (error) {
-      console.error('Ma\'lumotlarni olishda xato yuz berdi:', error);
-    }
-  };
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
-  fetchCategories();
-}, []);
+server.use(middlewares);
+server.use(router);
+
+server.listen(8080, () => {
+  console.log('JSON Server is running on port 8080');
+});
