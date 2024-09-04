067e3cc675752d6f7a778b07403d2b7f5e7510d8
mmfx001 <dilbekshermatov@gmail.com>
2024-09-04 15:57:00 +0500

    done

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 
-server.listen(8080, () => {
-  console.log('JSON Server is running on port 8080');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
 });
