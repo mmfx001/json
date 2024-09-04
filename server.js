16e82a9f8daac8a001d04603fa86123e19cf8faa
mmfx001 <dilbekshermatov@gmail.com>
2024-09-04 17:51:29 +0500

    don

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(router);
+server.use('/api', router); 
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
